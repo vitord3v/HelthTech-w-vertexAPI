d53b5068fcdb15b015dbdcff51da65c9ca86f09b
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-09 10:41:41 -0300

    render header on root page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 // pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
+import { Header } from "../components/Header";
 
 const colors = {
   brand: {
@@ -15,7 +16,10 @@ export const theme = extendTheme({ colors })
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+        <Header />
+        
+        <Component {...pageProps} />
+      
     </ChakraProvider>
   )
 }
