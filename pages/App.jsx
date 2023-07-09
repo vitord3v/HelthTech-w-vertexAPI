bcc73a8ce26e5e2d9d63558a821525c8391668a3
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-09 10:43:31 -0300

    [feat] create App page

---
+++ pages/App.jsx
@@ -0,0 +1,17 @@
+import { Card, CardHeader, CardBody, CardFooter, Textarea, Text, Box } from '@chakra-ui/react'
+
+
+const App = () => {
+    return (
+        <Box  mt='7rem' display='flex' alignItems='center' justifyContent='center'>
+            <Card maxW='500px' align={'center'} >
+                <CardHeader>
+                    <Text>Realize your search based in information</Text>
+                    <Textarea mt='2rem' placeholder='Have greate ideas 😊'/>
+                </CardHeader>
+            </Card> 
+        </Box>
+    )
+}
+
+export default App;
