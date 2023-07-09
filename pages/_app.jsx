a810c1b85a443fbc655c7bd53ada8c5f26b01d5d
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-09 09:01:48 -0300

    [fix] ajust _app.js page whit colors styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,23 @@
 // pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#7AEB80',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+export const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
 }
 
-export default MyApp;
+export default MyApp
