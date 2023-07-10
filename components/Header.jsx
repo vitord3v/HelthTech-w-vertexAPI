e10419013e6e444ede502431d2331ebeea4cc458
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-09 21:14:53 -0300

    [feat]import and render healthtech logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
-import { Flex, Box, Text, Tabs, TabList, Tab } from "@chakra-ui/react";
+import { Flex, Box, Text, Tabs, TabList, Tab, Image } from "@chakra-ui/react";
 import  Link  from 'next/link'
 
 export const Header = () => {
     return (
     <Flex as="header" align="center" p={4} maxW='1120px' justifyContent='space-between' margin='auto'>
-      <Box>
+      <Box display='flex' alignItems='center' justifyContent='center' gap='3' >
+        <Image src='/healthTechlogo.png' borderRadius='full' />
         <Text fontSize="xl" fontWeight="bold">HelthTech w/ AI Vertex</Text>
       </Box>
       <Box>
